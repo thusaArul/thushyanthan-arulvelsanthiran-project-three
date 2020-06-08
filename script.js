e8fa67d9317d26d44f18cac992b9d1480d692ae3
Thusa <thusa.arul@hotmail.com>
2020-06-08 16:07:04 -0400

    completed media query and final compilation of SASS

--- script.js
+++ script.js
@@ -3,117 +3,117 @@ const digimonApp = {};
 let images = {
     0: [
         {
-            image: './assets/digiEgg1.jpg',
+            image: './styles/images/digiEgg1.jpg',
             stage: 'Stage: DigiEgg'
         },
         {
-            image: './assets/digiEgg2.jpg',
+            image: './styles/images/digiEgg2.jpg',
             stage: 'Stage: DigiEgg'
         },
         {
-            image: './assets/digiEgg3.jpg',
+            image: './styles/images/digiEgg3.jpg',
             stage: 'Stage: DigiEgg'
         }
     ],
     1: [
         {
             name: 'Botamon',
-            image: './assets/fresh1.jpg',
+            image: './styles/images/fresh1.jpg',
             stage: 'Stage: Fresh'
         },
         {
             name: 'Punimon',
-            image: './assets/fresh2.jpg',
+            image: './styles/images/fresh2.jpg',
             stage: 'Stage: Fresh'
         },
         {
             name: 'Bubamon',
-            image: './assets/fresh3.jpg',
+            image: './styles/images/fresh3.jpg',
             stage: 'Stage: Fresh'
         }
     ],
     2: [
         {
             name: 'Koromon',
-            image: './assets/inTraining1.jpg',
+            image: './styles/images/inTraining1.jpg',
             stage: 'Stage: In-Training'
         },
         {
             name: 'Tsunemon',
-            image: './assets/inTraining2.jpg',
+            image: './styles/images/inTraining2.jpg',
             stage: 'Stage: In-Training'
         },
         {
             name: 'Mochimon',
-            image: './assets/inTraining3.jpg',
+            image: './styles/images/inTraining3.jpg',
             stage: 'Stage: In-Training'
         }
     ],
     3: [
         {
             name: 'Agumon',
-            image: './assets/rookie1.jpg',
+            image: './styles/images/rookie1.jpg',
             stage: 'Stage: Rookie'
         },
         {
             name: 'Gabumon',
-            image: './assets/rookie2.jpg',
+            image: './styles/images/rookie2.jpg',
             stage: 'Stage: Rookie'
         },
         {
             name: 'Tentomon',
-            image: './assets/rookie3.jpg',
+            image: './styles/images/rookie3.jpg',
             stage: 'Stage: Rookie'
         }
     ],
     4: [
         {
             name: 'Greymon',
-            image: './assets/champion1.jpg',
+            image: './styles/images/champion1.jpg',
             stage: 'Stage: Champion'
         },
         {
             name: 'Garurumon',
-            image: './assets/champion2.jpg',
+            image: './styles/images/champion2.jpg',
             stage: 'Stage: Champion'
         },
         {
             name: 'Kabuterimon',
-            image: './assets/champion3.jpg',
+            image: './styles/images/champion3.jpg',
             stage: 'Stage: Champion'
         }
     ],
     5: [
         {
             name: 'Metalgreymon',
-            image: './assets/ultimate1.jpg',
+            image: './styles/images/ultimate1.jpg',
             stage: 'Stage: Ultimate'
         },
         {
             name: 'Weregarurumon',
-            image: './assets/ultimate2.jpg',
+            image: './styles/images/ultimate2.jpg',
             stage: 'Stage: Ultimate'
         },
         {
             name: 'Megakabuterimon',
-            image: './assets/ultimate3.jpg',
+            image: './styles/images/ultimate3.jpg',
             stage: 'Stage: Ultimate'
         }
     ],
     6: [
         {
             name: 'Wargreymon',
-            image: './assets/mega1.jpg',
+            image: './styles/images/mega1.jpg',
             stage: 'Stage: Mega'
         },
         {
             name: 'Metalgarurumon',
-            image: './assets/mega2.jpg',
+            image: './styles/images/mega2.jpg',
             stage: 'Stage: Mega'
         },
         {
             name: 'Herculeskabuterimon',
-            image: './assets/mega3.jpg',
+            image: './styles/images/mega3.jpg',
             stage: 'Stage: Mega'
         }
     ]
@@ -123,6 +123,9 @@ digiModify = (i) => {
 
     // populate a box within each div
     $('div').css('border', '5px solid #ff8407')
+    .css('height', '225px')
+    .css('width', '225px')
+    .css('margin', '50px 5px 50px');
 
     // run random number generators for each div
     let randomNumber = Math.floor(Math.random() * 3);
@@ -139,7 +142,7 @@ digiModify = (i) => {
     let titleMaker = images[i][randomNumber]['name'];
     
     // append the image into the proper div
-    $(populater).append(`<img src= ${imageMaker} >`)
+    $(populater).append(`<img src= ${imageMaker} alt='An image of ${titleMaker}' height='215.4px' width='215.4px'>`)
 
     if($('.digiCodeOn').val() === "DigiCode"){
         // // append stage name below div
@@ -151,13 +154,13 @@ digiModify = (i) => {
         .append(`<p class='changeScript digiCodeStyle'> ${titleMaker} </p>`);
     }
 
-    // below is for the first div, the DIGIEGG, which is connected to the second div for which i = 1
+    // below is for the first div, the DIGIEGG, which is connected to the second div for which i = 1, same steps as above are followed
     if (i === 1) {
         let childSelectorDigiEgg = i;
         let populaterDigiEgg = 'main div:nth-child(' + [childSelectorDigiEgg] + ')';
         $(populaterDigiEgg).empty();
         let imageMakerDigiEgg = images[i - 1][randomNumber]['image'];
-        $(populaterDigiEgg).append(`<img src= ${imageMakerDigiEgg} >`)
+        $(populaterDigiEgg).append(`<img src= ${imageMakerDigiEgg} alt="An image of ${titleMaker}'s Digiegg" height='215.4px' width='215.4px'>`)
         let stageDigiEgg = images[i - 1][randomNumber]['stage'];
         $(populaterDigiEgg).append(`<p class='changeScript'> ${stageDigiEgg} </p>`);
     }
